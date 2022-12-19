ad8097c99c0b5ed38e14cfc1cd00db9583ad44b8
not-available-today <killer-am@protonmail.com>
2022-12-19 16:08:45 +0400

    Initial Project Add

---
+++ src/main/java/max/blackpegasus/mbppetclinic/MbpPetClinicApplication.java
@@ -0,0 +1,13 @@
+package max.blackpegasus.mbppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbpPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbpPetClinicApplication.class, args);
+    }
+
+}
