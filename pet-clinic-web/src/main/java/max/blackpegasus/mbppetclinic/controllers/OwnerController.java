fa50015bee2e02d8f2382b87f25003a95a68abae
not-available-today <killer-am@protonmail.com>
2022-12-27 13:20:34 +0400

    add owners page and controller. closes #13

--- pet-clinic-web/src/main/java/max/blackpegasus/mbppetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/max/blackpegasus/mbppetclinic/controllers/OwnerController.java
@@ -3,12 +3,12 @@ package max.blackpegasus.mbppetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
     @RequestMapping({"/","index","index.html"})
-    public String list(){
-        return "vets/index";
+    public String listOwners(){
+        return "owners/index";
     }
 }
